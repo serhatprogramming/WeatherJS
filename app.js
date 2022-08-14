f08e28632192643feeef78839b9f17e1d3094dd4
serhatprogramming <serhatprogramming@gmail.com>
2022-08-14 19:36:13 -0400

    local storage added

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const weatherObj = new Weather("wayne", "new jersey");
+const weatherLocation = new Storage();
+weatherLocation.getLocationData();
+const weatherObj = new Weather(weatherLocation.city, weatherLocation.state);
 const ui = new UI();
 
 document.addEventListener("DOMContentLoaded", getWeather);
+document.querySelector("#w-change").addEventListener("click", changeLocation);
+
+function changeLocation() {
+  const city = document.querySelector("#city");
+  const state = document.querySelector("#state");
+  weatherObj.changeLocation(
+    city.value,
+    state.options[state.selectedIndex].text
+  );
+  weatherLocation.setLocationData(
+    city.value,
+    state.options[state.selectedIndex].text
+  );
+  getWeather();
+  $("#locationModal").modal("hide");
+}
 
 function getWeather() {
   weatherObj
--- storage.js
+++ storage.js
@@ -1,6 +1,24 @@
 class Storage {
   constructor() {
-    this.state = state;
-    this.city = city;
+    this.state;
+    this.city;
+    this.defaultCity = "Manhattan";
+    this.defaultState = "New York";
+  }
+
+  getLocationData() {
+    if (localStorage.getItem("city") === null) {
+      this.city = this.defaultCity;
+      this.state = this.defaultState;
+      this.setLocationData(this.city, this.state);
+    } else {
+      this.city = localStorage.getItem("city");
+      this.state = localStorage.getItem("state");
+    }
+  }
+
+  setLocationData(city, state) {
+    localStorage.setItem("city", city);
+    localStorage.setItem("state", state);
   }
 }
